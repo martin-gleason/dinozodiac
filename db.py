859d6f933a2233c743bb813f6f0ac67e11f21bca
Martin Gleason <officergleason@gmail.com>
2020-02-18 07:45:00 -0600

    fix pyscopg2 connection error

---
+++ db.py
@@ -0,0 +1,31 @@
+#Python to PostgreSQL
+import psycopg2
+from configparser import ConfigParser
+
+
+def config(filename='db.ini', section='postgresql'):
+  parser = ConfigParser()
+  parser.read(filename)
+  db = {}
+  if parser.has_section(section):
+    params = parser.items(section)
+    for param in params:
+      db[param[0]] = param[1]
+  else:
+    raise Exception('Section {0} not found in the {1} file'.format(section, filename)
+  )
+  return db
+
+try:
+    params = config()
+    connection = psycopg2.connect(**params)
+
+    cursor = connection.cursor()
+    print ( connection.get_dsn_parameters(), "\n")
+
+    cursor.execute("SELECT * FROM scopes WHERE signid = 4;")
+    record = cursor.fetchall()
+    print("The sources are ", record, "\n So feel good!")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
